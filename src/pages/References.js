1d25d489aabe56ed859a8a670b6a4f87d973f38c
Anna A <patty.asprer@gmail.com>
2021-05-12 22:35:16 -0700

    Update reference page and links

--- src/pages/References.js
+++ src/pages/References.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 function References() {
 	return (
-		<div className='content test'>
-      		<h1>Useful refs I studied while making this:</h1>
+		<div className='references'>
+      		<div className='ptag-container content'>
+			  	<div>
+					<h2>Useful refs I used:</h2><br/>
+				</div>
+				<p className='ptags-proj inner'>
+					<li>
+						<a href="https://create-react-app.dev/docs/deployment/#github-pages">Deploying React Apps on GitHub Pages</a>
+					</li>
+					<li>
+						<a href={"https://learnwithparam.com/blog/dynamic-pages-in-react-router/"}>Dynamic Pages with React Router</a>
+					</li>
+					<li>
+						<a href={`https://www.youtube.com/watch?v=CXa0f4-dWi4&t=493s`}>A Great SideNav Tutorial</a>
+					</li>
+					<li>
+						<a href={`https://github.com/brainhubeu/react-carousel`}>Image Carousel Component</a>
+					</li>
+				</p>
+			</div>
     	</div>
 	)
 }
